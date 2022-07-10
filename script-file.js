d0a937a11db39e8a68823033e99b135e8b71c0c4
KittyTang02 <misskittytang@gmail.com>
2022-07-10 22:49:19 +0800

    🧮 Add pagination

--- script-file.js
+++ script-file.js
@@ -3,32 +3,83 @@ let calendarStartTime = moment();
 calendarStartTime.startOf("month");
 let calendarEndTime = moment();
 calendarEndTime.endOf("month");
-
-// calendarStartTime.subtract(1,'M');
-// calendarEndTime.subtract(1,'M').endOf('month');
-
-// 插入 x年x月
-addCalendarTitle(calendarStartTime);
-
-// 添加表头
-addTableHead();
-
-// 添加日历数字
-addTableNumbers(calendarStartTime, calendarEndTime);
-
-// Highlight TODAY
-const today = document.getElementById(moment().format('YYYY-MM-DD'));
-if (today) {today.classList.add('calendar-cell-today');}
-
-// Mark covid-testing days
 const covidTestingDays = ['2022-06-13','2022-06-14','2022-06-15','2022-06-16','2022-06-17','2022-06-18'];
-markCovidTestingDays();
 
-function addCalendarTitle(time) {
-    let calendarTitle = document.getElementById("calendar-title");
-    calendarTitle.innerText = time.format("YYYY[年]M[月]");
+renderCalendar(calendarStartTime, calendarEndTime);
+
+function renderCalendar(calendarStartTime, calendarEndTime) {
+    // 插入 x年x月
+    addCalendarTitle(calendarStartTime);
+
+    // Add buttons
+    addCalendarButtons();
+
+    // 添加标题行
+    addTableHead();
+
+    // 添加日历数字
+    addTableNumbers(calendarStartTime, calendarEndTime);
+
+    // Highlight TODAY
+    const today = document.getElementById(moment().format('YYYY-MM-DD'));
+    if (today) {today.classList.add('calendar-cell-today');}
+
+    // Mark covid-testing days
+    markCovidTestingDays();
+
     return;
 }
+
+function addCalendarTitle(time) {
+    const calendarTitle = document.createElement("div");
+    calendarTitle.innerText = time.format("YYYY[年]M[月]");
+    calendarTitle.classList.add("calendar-title");
+    calendarTitle.classList.add("hiragino-16");
+    document.getElementById("calendar-title-and-buttons").appendChild(calendarTitle);
+    return;
+}
+
+function addCalendarButtons(){
+    addCalendarButtonArrowLeft();
+    addCalendarButtonCurrentMonth();
+    addCalendarButtonArrowRight();
+    return;
+};
+
+function addCalendarButtonArrowLeft() {
+    const button = document.createElement("div");
+    button.id = "calendar-button-left";
+    button.classList.add("calendar-button");
+    const arrowLeft = document.createElement("div");
+    arrowLeft.classList.add("arrow-left");
+    button.appendChild(arrowLeft);
+    button.addEventListener("click", goToPreviousMonth);
+    document.getElementById("calendar-title-and-buttons").appendChild(button);
+    return;
+};
+
+function addCalendarButtonCurrentMonth() {
+    const button = document.createElement("div");
+    button.id = "calendar-button-current";
+    button.classList.add("calendar-button");
+    const currentCircle = document.createElement("div");
+    currentCircle.classList.add("current-circle");
+    button.appendChild(currentCircle);
+    button.addEventListener("click", goToCurrentMonth);
+    document.getElementById("calendar-title-and-buttons").appendChild(button);
+}
+
+function addCalendarButtonArrowRight() {
+    const button = document.createElement("div");
+    button.id = "calendar-button-right";
+    button.classList.add("calendar-button");
+    const arrowRight = document.createElement("div");
+    arrowRight.classList.add("arrow-right");
+    button.appendChild(arrowRight);
+    button.addEventListener("click", goToNextMonth);
+    document.getElementById("calendar-title-and-buttons").appendChild(button);
+    return;
+};
 
 function addTableHead() {
     const WeekDays = ["一", "二", "三", "四", "五", "六", "日"];
@@ -37,6 +88,7 @@ function addTableHead() {
         tableCell.innerText = DayOfWeek;
         document.getElementById("calendar-table").appendChild(tableCell);
     }
+    return;
 }
 
 function addTableNumbers(time, endTime) {
@@ -56,6 +108,9 @@ function addTableNumbers(time, endTime) {
         time.add(1, "d");
     }
 
+    // Set calendarStartTime to calendar month
+    time.subtract(1, 'd');
+
     // Move the first day to right column
     document.getElementById(firstDayId).style.gridColumnStart = columnNumber;
     return;
@@ -71,5 +126,37 @@ function markCovidTestingDays() {
             container.insertBefore(markPattern,container.firstChild);
         }
     }
+    return;
+}
+
+function goToPreviousMonth(){
+    calendarEndTime.subtract(1, 'M');
+    calendarStartTime.subtract(1, 'M').startOf("month");
+    clearCalendar();
+    renderCalendar(calendarStartTime, calendarEndTime);
+    return;
+}
+
+function goToCurrentMonth() {
+    calendarStartTime = moment();
+    calendarStartTime.startOf("month");
+    calendarEndTime = moment();
+    calendarEndTime.endOf("month");
+    clearCalendar();
+    renderCalendar(calendarStartTime, calendarEndTime);
+    return;
+}
+
+function goToNextMonth(){
+    calendarEndTime.add(1, 'M');
+    calendarStartTime.add(1, 'M').startOf("month");
+    clearCalendar();
+    renderCalendar(calendarStartTime, calendarEndTime);
+    return;
+}
+
+function clearCalendar() {
+    document.getElementById("calendar-title-and-buttons").innerHTML = "";
+    document.getElementById("calendar-table").innerHTML = "";
     return;
 }
